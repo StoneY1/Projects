e40ea335bf7cf16a7440333b9941a903c177d096
StoneY1 <35155689+StoneY1@users.noreply.github.com>
2019-03-23 13:21:18 -0400

    starting Collatz Conjecture algorithm

---
+++ Collatz_Conjecture/Cpp/collatz_main.cpp
@@ -0,0 +1,6 @@
+/*
+Stone Yun 2019.
+For any number n > 1, will find how many operations it takes to get to 1 as described in README
+*/
+include <iostream>
+using namespace std;
---
+++ Collatz_Conjecture/python/collatz_main.py
@@ -0,0 +1,5 @@
+"""
+Stone Yun 2019.
+For any number n > 1, will find how many operations it takes to get to 1 as described in README
+"""
+import numpy as np
