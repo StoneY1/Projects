87905165bdf19f9bfc7cca0893ce12701a1b6c57
StoneY1 <35155689+StoneY1@users.noreply.github.com>
2019-03-23 18:05:27 -0400

    created working solution. Need to incorporate decorators next

--- Collatz_Conjecture/python/collatz_main.py
+++ Collatz_Conjecture/python/collatz_main.py
@@ -3,3 +3,23 @@ Stone Yun 2019.
 For any number n > 1, will find how many operations it takes to get to 1 as described in README
 """
 import numpy as np
+from helpers import args
+from step import *
+
+
+def run_collatz(num):
+    steps = 0
+    #TODO: create decorators that will count when the step functions are called
+    while num != 1:
+        if num%2 == 1:
+            num = step_odd(num)
+        else:
+            num = step_even(num)
+        steps += 1
+    return steps
+
+if __name__ == "__main__":
+    args.add_arg('-n','--number', required=True)
+    num = int(args.get_arg('number'))
+    s = run_collatz(num)
+    print(f"Starting from {num} it took {s} steps to get to 1")
---
+++ Collatz_Conjecture/python/step.py
@@ -0,0 +1,12 @@
+"""
+Stone Yun 2019
+Functions here will check if *n* is odd or even and then operate accordingly.
+"""
+
+def step_odd(n):
+    """if odd, multiply by 3 and add 1"""
+    return (n*3+1)
+
+def step_even(n):
+    """if even, divide by 2"""
+    return n/2
